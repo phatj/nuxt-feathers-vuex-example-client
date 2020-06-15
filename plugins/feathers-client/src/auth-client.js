157b0fafab4bb7717b1e01a4afc2a43019f1a208
Jamal Shah <jshah@fluxworkstudios.com>
2020-06-14 20:52:52 -0400

    fix:  move auth client config into request scope

--- plugins/feathers-client/src/auth-client.js
+++ plugins/feathers-client/src/auth-client.js
@@ -1,17 +1,19 @@
 import init, { MemoryStorage } from '@feathersjs/authentication-client';
 import { CookieStorage } from 'cookie-storage';
 
-const authClientConfig = {
-  storage: process.client
-    ? // Use cookies on the client
-      new CookieStorage({
-        path: '/',
-        secure: process.env.USE_SSL
-      })
-    : // Use memory on the server
-      new MemoryStorage(),
-  storageKey: process.env.COOKIE_NAME
-};
-
 // Wrapped in a function for request separation
-export default () => init(authClientConfig);
+export default () => {
+  const authClientConfig = {
+    storage: process.client
+      ? // Use cookies on the client
+        new CookieStorage({
+          path: '/',
+          secure: process.env.USE_SSL
+        })
+      : // Use memory on the server; no access to document.cookie
+        new MemoryStorage(),
+    storageKey: process.env.COOKIE_NAME
+  };
+
+  init(authClientConfig);
+};
